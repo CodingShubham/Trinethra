3a96d0660809d7457d87aec74b3cefe8e146a4bd
Shubham <ss9824473@gmail.com>
2026-04-30 18:56:02 +0530

    Tested Ollama response in server

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,38 @@ app.get('/', (req, res) => {
   res.json({ message: 'Trinethra backend is running!' })
 })
 
+
+app.get("/olama-test",async(req,res)=>{
+
+    try{
+
+      const response=await fetch("http://localhost:11434/api/generate",{
+
+        method:"POST",
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify({
+          model:'llama3.2',
+          prompt:'Who is prime minister of India in 2003',
+          stream:false
+        })
+
+      })
+
+      const data=await response.json();
+      res.json({ollama_response: data.response})
+
+    }
+
+    catch(error){
+
+         res.json({ error: 'Ollama not working' })
+
+    }
+
+
+})
+
+
 app.listen(port,()=>{
     console.log(`server listening on port ${port}`)
 })
